5539687afb446915393d5260914c281e66d662f4
DavidThapa <daybeat06@gmail.com>
2023-12-26 11:14:17 +0545

    Modular coding for Data Ingestion

---
+++ src/cnnClassifier/entity/config_entity.py
@@ -0,0 +1,11 @@
+# Entity is nothing but a return type of a function. Below Class will be a custom entity.
+
+from dataclasses import dataclass
+from pathlib import Path
+
+@dataclass(frozen=True)
+class DataIngestionConfig:
+    root_dir: Path
+    source_URL: str
+    local_data_file: Path
+    unzip_dir: Path
